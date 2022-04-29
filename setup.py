0c44e4bee1508575398b337c594b83890a34811a
Amir Sharapov <amir@amirfounder.com>
2022-04-29 18:00:16 -0500

    typo

--- setup.py
+++ setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'daos @ git+https://github.com/amirfounder/ml-studies-daos',
-        'feedparser~=6.0.8',
-        'bs4~=0.0.1',
+        'feedparser>=6.0.8',
+        'bs4>=0.0.1',
     ])
