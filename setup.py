6b69a4766e91dc5f37f5163820c0df750e269848
Amir Sharapov <amir@amirfounder.com>
2022-04-29 14:33:47 -0500

    setup fix

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='daos',
-    version='0.1.11',
+    name='services',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
+        'dao @ git+https://github.com/amirfounder/ml-studies-daos'
         'feedparser~=6.0.8',
         'bs4~=0.0.1',
     ])
