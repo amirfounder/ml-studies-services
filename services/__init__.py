d172d394d4ef5d7f964628dff2af0b87327fcc8c
Amir Sharapov <amir@amirfounder.com>
2022-04-30 09:40:07 -0500

    testing

--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,9 @@
+from typing import Callable
+
 from .data_extractors import *
 
 SERVICES = []
 
 for name, variable in globals().items():
-    if isinstance(variable, callable) and variable not in SERVICES:
+    if isinstance(variable, Callable) and variable not in SERVICES:
         SERVICES.append(variable)
