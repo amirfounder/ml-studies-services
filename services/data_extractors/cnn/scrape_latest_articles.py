3100f6fed3aa2e4e12ed58097e7472b618be9cb9
Amir Sharapov <amir@amirfounder.com>
2022-05-01 07:53:48 -0500

    updated the server

--- services/data_extractors/cnn/scrape_latest_articles.py
+++ services/data_extractors/cnn/scrape_latest_articles.py
@@ -19,18 +19,16 @@ def scrape_latest_cnn_articles(**kwargs):
 
     print(f'Rss Entries to scrape : {len(rss_entries)}')
 
-    urls_opened_in_current_batch = 0
+    batches = [rss_entries[i:i + scrapes_per_batch] for i in range(0, len(rss_entries), scrapes_per_batch)]
 
-    for rss_entry in rss_entries:
-        try:
-            webbrowser.open_new_tab(rss_entry.url)
-            urls_opened_in_current_batch += 1
+    for batch in batches:
+        for rss_entry in batch:
+            try:
+                webbrowser.open_new_tab(rss_entry.url)
+                report['succeeded'] += 1
 
-            if scrapes_per_batch == urls_opened_in_current_batch:
-                time.sleep(ms_interval_between_batches / 1000)
+            except Exception as e:
+                print(f'Exception has been raised : {str(e)}')
+                report['failed'] += 1
 
-            report['succeeded'] += 1
-
-        except Exception as e:
-            print(f'Exception has been raised : {str(e)}')
-            report['failed'] += 1
+        time.sleep(ms_interval_between_batches / 1000)
